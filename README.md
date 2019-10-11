e0cdb520514d5909008fff48a982fa17dab585ac
Candace <candacerogers114@gmail.com>
2019-10-11 14:46:25 -0400

    Adding parameter to affirm dev env for console.logs

--- README.md
+++ README.md
@@ -11,8 +11,13 @@ $ npm install 'git@github.com:blavity/fingerprint.git#master'
 ```js
 const fingerprint = require("@blavity/fingerprint");
 
-fingerprint(this, https://api.staging.blavity.com/v1/fingerprint, https://blavity.com/ );
-//In developer tools you'll see => "your fp:  9eea5059858c3274041xjrty40f"
+fingerprint(this, https://api.staging.blavity.com/v1/fingerprint, https://blavity.com/, true );
+
+> Passes entire vueInstance into the function, will need to pass in the this keyword and it will destructure below
+> Pass the endpoint in the 2nd param to keep the endpoint dynamic
+> Pass the slug in the 3rd param to keep the function site agnostic
+> Pass a true or false in the 4th param to affirm that you are in the development enviornment or not
+> In developer tools you'll see => "your fp:  9eea5059858c3274041xjrty40f"
 
 fingerprint(9eea5059858c3274041xjrty40f);
 //=> Uncaught TypeError: This fingerprint is already taken!
--- index.js
+++ index.js
@@ -2,10 +2,7 @@
 const Fingerprint2 = require('script/fingerprintjs2');
 
 module.exports = {
-  fingerprint : (vueInstance, endpoint, slug) => {
-    //Passes entire vueInstance into the function, will need to pass in the this keyword and it will destructure below
-    //Pass the endpoint in the 2nd param to keep the endpoint dynamic
-    //Pass the slug in the 3rd param to keep the function site agnostic
+  fingerprint : (vueInstance, endpoint, slug, isDev) => {
     const{ $axios, $route } = vueInstance
     Fingerprint2.get((components) => {
       const murmur = Fingerprint2.x64hash128(components.map((pair) => { return pair.value }).join(), 31)
@@ -17,9 +14,15 @@ module.exports = {
         action: 'visited',
         created_at: new Date()
       }, {headers: headers})
-      .then(response => {console.log('200: Pushed fingerprint to database successfully!', response.data)})
+      .then(response => {
+        if (isDev){
+          console.log('200: Pushed fingerprint to database successfully!', response.data)
+        }
+      })
       .catch(e => {
-        console.log(e)
+        if (isDev){
+          console.log(e)
+        }
       })
     })
   }
